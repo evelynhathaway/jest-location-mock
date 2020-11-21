efc9fc589d97ae70d96f4ad520c50a6be299a5a3
Evelyn Hathaway <git@evelyn.dev>
2020-11-21 13:51:39 -0800

    Docs: Meta update, readme, types, license

--- src/jest.d.ts
+++ src/jest.d.ts
@@ -1,6 +1,6 @@
 declare namespace jest {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	interface Matchers<R, T> {
-		toBeAt(url: string, base?: string | URL): R;
+		toBeAt(url: string | URL, base?: string | URL): R;
 	}
 }
